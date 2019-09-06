cbc56bbce27bd6a6929cb38484283c70c0b3141c
Tom Selfin <tomselfin@Toms-MacBook-Pro.local>
2019-09-06 14:37:39 +0300

    implemented training format cycle

--- main.py
+++ main.py
@@ -1,19 +1,37 @@
 import gym
+import numpy as np
 from neat import NEAT
+import matplotlib.pyplot as plt
+
 
 env = gym.make("CartPole-v1")
 POPULATION_SIZE = 10
-INPUTS = env.observation_space
-agents = NEAT(POPULATION_SIZE, 10, 10)
+INPUTS = env.observation_space.shape[0]
+OUTPUTS = env.action_space.n
+neat_manager = NEAT(POPULATION_SIZE, INPUTS, OUTPUTS)
+ENV_STEPS = 1000
+GENERATIONS = 10
 
-"""
-observation = env.reset()
-for _ in range(1000):
-  env.render()
-  action = env.action_space.sample() # your agent here (this takes random actions)
-  observation, reward, done, info = env.step(action)
-
-  if done:
-    observation = env.reset()
+avg_scores = []
+for gen in range(GENERATIONS):
+    agent_scores = []
+    for agent in neat_manager.iter_agents():
+        observation = env.reset()
+        score = 0
+        for _ in range(ENV_STEPS):
+            env.render()
+            action = agent.predict(np.array(observation)).argmax() # your agent here (this takes random actions)
+            observation, reward, done, info = env.step(action)
+            score += reward
+            if done:
+                observation = env.reset()
+                break
+        agent_scores.append(score)
+    
+    # get a new generation
+    avg_scores.append(np.average(agent_scores))
+    neat_manager.new_generation(agent_scores)
 env.close()
-"""
+
+plt.plot(range(1, GENERATIONS+1), avg_scores)
+plt.show()
