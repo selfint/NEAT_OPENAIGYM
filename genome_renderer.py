f4b454b774d6a0e5cef9a563ef226a43da3c2c12
Tom Selfin <selfint@gmail.com>
2019-09-16 19:07:26 +0300

    adds rendering to a directory

--- genome_renderer.py
+++ genome_renderer.py
@@ -1,10 +1,10 @@
 from data_types import Genome
 import pygraphviz as pgv
-
+import os
 
 class GenomeRenderer:
 
-    def __init__(self, genome: Genome, filename: str = 'genome_render.png'):
+    def __init__(self, genome: Genome, filename: str = 'genome_render.png', dirname: str = 'renders'):
         """Draws a genome in png format using pygraphviz
         
         Arguments:
@@ -14,4 +14,7 @@ class GenomeRenderer:
         for (src, dst) in [(inn.src, inn.dst) for inn in genome.innovations]:
             graph.add_edge(src, dst)
         graph.layout()
-        graph.draw(filename)
+
+        if not os.path.exists(dirname):
+            os.mkdir(dirname)
+        graph.draw(path = os.path.join(dirname, filename))
